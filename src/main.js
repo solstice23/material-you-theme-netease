5da78d1314bc59429c44c7ae13041b530c45d838
solstice23 <solstice233@gmail.com>
2022-12-20 23:50:49 +0800

    Playlist header

--- src/main.js
+++ src/main.js
@@ -247,6 +247,55 @@ const removeRedundantPlaylists = () => {
 	}
 }
 
+let lastPlaylistTitle = "";
+const titleSizeController = document.createElement('style');
+titleSizeController.innerHTML = '';
+document.head.appendChild(titleSizeController);
+const recalculateTitleSize = (forceRefresh = false) => {
+	const title = document.querySelector('.g-mn .m-info .tit .name h2 .f-ust');
+	if (!title) {
+		return;
+	}
+	if (title.innerText === lastPlaylistTitle && !forceRefresh) {
+		return;
+	}
+	lastPlaylistTitle = title.innerText;
+	const text = title.innerText;
+	const testDiv = document.createElement('div');
+	testDiv.style.position = 'absolute';
+	testDiv.style.top = '-9999px';
+	testDiv.style.left = '-9999px';
+	testDiv.style.width = 'auto';
+	testDiv.style.height = 'auto';
+	testDiv.style.whiteSpace = 'nowrap';
+	testDiv.innerText = text;
+	document.body.appendChild(testDiv);
+
+	const maxThreshold = 80;
+	const minThreshold = 24;
+	const targetWidth = document.querySelector('.g-mn .m-info .tit .name').clientWidth - 30;
+
+	let l = 1, r = 61;
+	while (l < r) {
+		const mid = Math.floor((l + r) / 2);
+		testDiv.style.fontSize = `${mid}px`;
+		const width = testDiv.clientWidth;
+		if (width > targetWidth) {
+			r = mid;
+		} else {
+			l = mid + 1;
+		}
+	}
+	let fontSize = l - 1;
+	fontSize = Math.max(Math.min(fontSize, maxThreshold), minThreshold);
+	document.body.removeChild(testDiv);
+	titleSizeController.innerHTML = `
+		.g-mn .m-info .tit .name h2 {
+			font-size: ${fontSize}px !important;
+		}
+	`;
+}
+
 
 plugin.onLoad(async (p) => {
 	pluginPath = p.pluginPath;
@@ -283,7 +332,7 @@ plugin.onLoad(async (p) => {
 		
 		new MutationObserver(() => {
 			updateTimeIndicatorPosition();
-		}).observe(document.querySelector('#main-player .speed'), { childList: true});
+		}).observe(document.querySelector('#main-player .speed'), { childList: true });
 	});
 
 	// Greeting and two recomment playlists
@@ -296,6 +345,23 @@ plugin.onLoad(async (p) => {
 			initRecommendPlaylists();
 			removeRedundantPlaylists();
 		}).observe(dom, { childList: true, subtree: true });
+	});
+
+
+	waitForElement('.g-mn', (dom) => {
+		new MutationObserver(() => {
+			// action buttons innerText to css property
+			const buttons = document.querySelectorAll('.u-ibtn5');
+			for (let button of buttons) {
+				button.style.setProperty('--text', `'${button.innerText}'`);
+			}
+			
+			// playlist title size recalculation
+			recalculateTitleSize();
+		}).observe(dom, { childList: true, subtree: true });
+	});
+	window.addEventListener('resize', () => {
+		recalculateTitleSize();
 	});
 
 });
