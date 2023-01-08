f5120ef139fb3305310e2be5bda0b6dc17ec791a
solstice23 <solstice233@gmail.com>
2023-01-08 16:25:02 +0800

    More components support

--- src/main.js
+++ src/main.js
@@ -406,6 +406,11 @@ plugin.onLoad(async (p) => {
 			for (let button of buttons) {
 				button.style.setProperty('--text', `'${button.innerText}'`);
 			}
+			// hide text 单曲数，专辑数，MV 数
+			const artistInfoItems = document.querySelectorAll('.g-mn .m-info .inf .item');
+			for (let item of artistInfoItems) {
+				item.style.setProperty('--number', `'${item.innerHTML.match(/(\d+)/)[1]}'`);
+			}
 			
 			// playlist title size recalculation
 			recalculateTitleSize();
