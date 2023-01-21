808c7c9e0d582df529b8383ac7886932f3322309
solstice23 <solstice233@gmail.com>
2023-01-21 17:21:25 +0800

    Add support for podcast page support lists with cover

--- src/main.js
+++ src/main.js
@@ -338,13 +338,14 @@ const scrollToCurrentPlaying = () => {
 			<span class="u-tit f-ff2 errTxt">定位失败</span>`);
 		return;
 	}
+	const itemHeight = parseInt(getComputedStyle(document.querySelector('.m-plylist')).getPropertyValue('--item-height') || 50);
 	const currentPlayingIndex =
 		Array.from(currentPlaying.parentNode.parentNode.children).indexOf(currentPlaying.parentNode) * 20 +
 		Array.from(currentPlaying.parentNode.children).indexOf(currentPlaying) + 1;
 	const currentPlayingOffset = 
 		document.querySelector('.m-plylist ul').offsetTop -
 		(document.documentElement.clientHeight / 2) +
-		currentPlayingIndex * 50 + 100;
+		currentPlayingIndex * itemHeight + 100;
 	document.querySelector('.g-mn:not(.better-ncm-manager)').scrollTo(0, currentPlayingOffset);
 }
 
@@ -466,19 +467,19 @@ plugin.onLoad(async (p) => {
 		listScrollVisibilityController.innerHTML = '';
 		document.head.appendChild(listScrollVisibilityController);
 		dom.addEventListener('scroll', () => {
-			console.log('scroll');
 			const listContainer = document.querySelector('.m-plylist-pl2 ul .lst');
 			if (!listContainer) {
 				listScrollVisibilityController.innerHTML = '';
 				return;
 			}
+			const itemHeight = parseInt(getComputedStyle(listContainer).getPropertyValue('--item-height') || 50);
 			const topOfList = Math.max(-(listContainer.getBoundingClientRect().top - 60), 0);
 			const listLength = listContainer.childElementCount;
-			const currentVisibleChild = Math.floor(topOfList / 1000) + 1;
+			const currentVisibleChild = Math.floor(topOfList / (itemHeight * 20)) + 1;
 			const l = Math.max(currentVisibleChild - 2, 1), r = Math.min(currentVisibleChild + 2, listLength);
 			let css = `.m-plylist-pl2 ul .lst {
-				padding-top: ${(l - 1) * 1000}px !important;
-				padding-bottom: ${(listLength - r) * 1000}px !important;
+				padding-top: ${(l - 1) * (itemHeight * 20)}px !important;
+				padding-bottom: ${(listLength - r) * (itemHeight * 20)}px !important;
 				counter-reset: tlistorder ${(l - 1) * 20} !important;
 			}`;
 			for (let i = 1; i <= l - 1; i++) {
@@ -490,7 +491,7 @@ plugin.onLoad(async (p) => {
 			if (listScrollVisibilityController.innerHTML != css) {
 				listScrollVisibilityController.innerHTML = css;
 			}
-		});
+		}, { passive: true });
 	});
 
 	// Fix toolbar button offset
