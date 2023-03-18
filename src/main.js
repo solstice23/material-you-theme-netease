0855c19c54c460979df7e41edff79b319778d6e2
solstice23 <solstice233@gmail.com>
2023-03-18 20:53:37 +0800

    Separate list scroll fix

--- src/main.js
+++ src/main.js
@@ -651,42 +651,6 @@ plugin.onLoad(async (p) => {
 		initSettingMenu();
 	});
 
-	// Fix playlist scroll
-	waitForElement('.g-mn', (dom) => {
-		const listScrollVisibilityController = document.createElement('style');
-		listScrollVisibilityController.innerHTML = '';
-		document.head.appendChild(listScrollVisibilityController);
-		const recalc = () => {
-			const listContainer = document.querySelector('.m-plylist-pl2 ul .lst');
-			if (!listContainer) {
-				listScrollVisibilityController.innerHTML = '';
-				return;
-			}
-			const itemHeight = parseInt(getComputedStyle(listContainer).getPropertyValue('--item-height') || 50);
-			const topOfList = Math.max(-(listContainer.getBoundingClientRect().top - 60), 0);
-			const listLength = listContainer.childElementCount;
-			const currentVisibleChild = Math.floor(topOfList / (itemHeight * 20)) + 1;
-			const l = Math.max(currentVisibleChild - 2, 1), r = Math.min(currentVisibleChild + 2, listLength);
-			let css = `.m-plylist-pl2 ul .lst {
-				padding-top: ${(l - 1) * (itemHeight * 20)}px !important;
-				padding-bottom: ${(listLength - r) * (itemHeight * 20)}px !important;
-				counter-reset: tlistorder ${(l - 1) * 20} !important;
-			}`;
-			for (let i = 1; i <= l - 1; i++) {
-				css += `.m-plylist-pl2 ul .lst > div:nth-child(${i}) { display: none !important; }`;
-			}
-			for (let i = r + 1; i <= listLength; i++) {
-				css += `.m-plylist-pl2 ul .lst > div:nth-child(${i}) { display: none !important; }`;
-			}
-			if (listScrollVisibilityController.innerHTML != css) {
-				listScrollVisibilityController.innerHTML = css;
-			}
-			listContainer.classList.add('mdtheme-scroll-management');
-		};
-		dom.addEventListener('scroll', recalc , { passive: true });
-		document.addEventListener('resize', recalc);
-	});
-
 	// Fix toolbar button offset
 	waitForElement('.g-sd', (dom) => {
 		const toolbarLeftPart = document.querySelector('header.g-hd .m-leftbox');
